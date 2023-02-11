f1746d71fc4560ca47a1252d7075029335fd5608
sabbir1021 <5boysbd@hmail.com>
2023-02-11 16:56:13 +0600

    slider add search add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,25 @@
-import {Card, Col, Row, Container, Button} from 'react-bootstrap';
+import {Card, Col, Row, Container, Button, Carousel, Pagination} from 'react-bootstrap';
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
+import cover1 from '../images/HeaderCover1.jpg';
+import cover2 from '../images/HeaderCover2.jpg';
+import cover3 from '../images/HeaderCover3.jpg';
+
 
 function Home() {
 
-  const [products, setProducts] = useState([])
+  const [query, setQuery] = useState("");
+  const [products, setProducts] = useState([]);
+  const [pagination, setPagination] = useState("");
 
-  const fetchData = () => {
-    fetch('https://api.superpharmacycompounding.com.au/api/v1.0/products/user/products/?'+ new URLSearchParams({
-      show_in_search: false
-  }))
+  const fetchData = async () => {
+    if (!pagination) {
+      setPagination("page=1&page_size=12")
+    }
+    console.log('-----------',pagination);
+    await fetch('https://api.superpharmacycompounding.com.au/api/v1.0/products/user/products/?'+ `${pagination}`+ '&' + new URLSearchParams({
+      show_in_search: false}) + `&search=${query}`
+      )
       .then(response => {
         return response.json()
       })
@@ -20,7 +30,7 @@ function Home() {
 
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [query, pagination])
 
   const shoot = (a) => {
     let products = JSON.parse(localStorage.getItem('cart'));
@@ -60,39 +70,85 @@ function Home() {
   }
 
   return (
-    <Container className='mt-3 mb-5'>
-      <h2 className='text-center mb-3'>Product List</h2>
-      <nav class="navbar navbar-light bg-light">
-        <form >
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </nav>
-      <Row xs={1} md={3} className="g-4">
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={cover1}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={cover2}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={cover3}
+            alt="First slide"
+          />
+        </Carousel.Item>
 
-        {products.length > 0 && (
-              products.map(product => (
-                <Col>
-                  <Card>
-                    <Card.Img variant="top" src={product.thumbnail} />
-                    <Card.Body>
-                      <h2 className='text-success'>${product.price}</h2>
-                      <Card.Title>{product.name}</Card.Title>
-                      <Card.Text>
-                          {product.category.name}
-                      </Card.Text>
-                      
-                      <Button as={Link} to={`/product/${product.id}`} variant="primary">View details</Button>
-                      <Button onClick={() => shoot(product)} variant="primary">Add To Cart</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                
-            ))
-          )}
+      </Carousel>
+      <Container className='mt-5 mb-5'>
+        <h2 className='text-center mb-3'>Product List</h2>
+        <div class="row">
+          <div class="col-md-5 mx-auto">
+              <div class="input-group">
+                  <input onChange={(e)=> setQuery(e.target.value)} class="form-control " type="text" />
+                  {/* <span class="input-group-append">
+                      <button class="btn btn-outline-success" type="button">
+                          Search
+                      </button>
+                  </span> */}
+              </div>
+          </div>
+        </div>
+
+        <Row xs={1} md={3} className="g-4 mt-3">
+
+          {products.length > 0 && (
+                products.map(product => (
+                  <Col>
+                    <Card>
+                      <Card.Img variant="top" src={product.thumbnail} />
+                      <Card.Body>
+                        <h2 className='text-success'>${product.price}</h2>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>
+                            {product.category.name}
+                        </Card.Text>
+                        <div class="d-grid mt-2">
+                          <Button onClick={() => shoot(product)} variant="success">Add To Cart</Button>
+                        </div>
+
+                        <div class="d-grid mt-2">
+                          <Button className='float-right' as={Link} to={`/product/${product.id}`} variant="primary">View details</Button>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
                   
-    </Row>
-    </Container>
+              ))
+            )}
+                    
+        </Row>
+        <Pagination className='mt-5 justify-content-center'>
+          <Pagination.First disabled/>
+          <Pagination.Prev  disabled/>
+          <Pagination.Ellipsis disabled />
+          <Pagination.Item active>{12}</Pagination.Item>
+          <Pagination.Ellipsis disabled />
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
+      </Container>
+    </div>
   );
 }
 
