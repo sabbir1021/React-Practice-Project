fd0291a2ca542b2e7201f5b703e0627f492d979a
sabbir1021 <5boysbd@gmail.com>
2023-02-27 13:43:42 +0600

    loader add on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,27 +2,38 @@ import {Card, Col, Row, Container, Button} from 'react-bootstrap';
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import './Style.css';
+import { useNavigate } from "react-router-dom";
+
 
 function Profile() {
 
+  const [loading, setLoading] = useState(false)
   const [user, setUser] = useState([])
   const token = localStorage.getItem('access_token');
 
-  const fetchData = () => {
-    fetch('https://api.superpharmacycompounding.com.au/api/v1.0/accounts/admin/profile/' , {
+  const navigate = useNavigate();
+
+  const fetchData = async () => {
+    try {
+      let res = await fetch("https://api.superpharmacycompounding.com.au/api/v1.0/accounts/admin/profile/", {
         method: "GET",
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`
         }
-      })
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setUser(data.data)
-      })
+      });
+      let data = await res.json();
+      if (res.status === 401) {
+          navigate("/login");
+          setLoading(true)
+      } else {
+          setUser(data.data)
+          setLoading(true)
+          }
+      } catch (err) {
+          console.log(err);
+      }
   }
 
   useEffect(() => {
@@ -31,22 +42,35 @@ function Profile() {
 
   return (
     <Container className='mt-3 mb-5'>
+      {!loading? 
+      <>
+        <div class="loader-circle">
+          <p class="loader-content">LOADING</p>
+          <div class="loader-line-mask">
+            <div class="loader-line"></div>
+          </div>
+        </div>
+      </>
+      :
+      <>
       <h2 className='text-center mb-3'>Profile</h2>
-        <Row className="justify-content-center mt-2">
-            <Col md={4}>
-                <Card>
-                <img className='profile rounded-circle mx-auto mt-3' src={user.profile_pic_url} />
-                <Card.Body>
-                    <Card.Title className='text-center'>Name: {user.first_name} {user.last_name}</Card.Title>
-                    <Card.Title className='text-center'>Phone: {user.phone_number}</Card.Title>
-                    
-                </Card.Body>
-                <div className=" mt-2 mb-2 text-center">
-                  <Button className='mx-auto' as={Link} to={'/profile-edit'} variant="primary">Edit Profile</Button>
-                </div>
-                </Card>
-            </Col>
-        </Row>
+      <Row className="justify-content-center mt-2">
+          <Col md={4}>
+              <Card>
+              <img className='profile rounded-circle mx-auto mt-3' src={user.profile_pic_url} />
+              <Card.Body>
+                  <Card.Title className='text-center'>Name: {user.first_name} {user.last_name}</Card.Title>
+                  <Card.Title className='text-center'>Phone: {user.phone_number}</Card.Title>
+                  
+              </Card.Body>
+              <div className=" mt-2 mb-2 text-center">
+                <Button className='mx-auto' as={Link} to={'/profile-edit'} variant="primary">Edit Profile</Button>
+              </div>
+              </Card>
+          </Col>
+      </Row></>
+      }
+      
     </Container>
   );
 }
